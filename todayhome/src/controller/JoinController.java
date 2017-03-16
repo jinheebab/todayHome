f5741a086e428e19efef7081908afad965a99d12
user <user@Hipster-HP>
2017-03-16 16:21:16 +0900

    d

--- todayhome/src/controller/JoinController.java
+++ todayhome/src/controller/JoinController.java
@@ -1,5 +1,22 @@
 package controller;
 
-public class JoinController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import model.JoinDao;
+
+@Controller
+public class JoinController {
+	@Autowired
+	JoinDao jDao;
+	
+	@RequestMapping("view/join")
+	public ModelAndView joinHandler(){
+		ModelAndView mav = new ModelAndView("g_index");
+		mav.addObject("main", "/join/join");
+		System.out.println("들어옴");
+		return mav;
+	}
 }
--- todayhome/src/model/JoinDao.java
+++ todayhome/src/model/JoinDao.java
@@ -1,5 +1,29 @@
 package model;
 
+import java.util.HashMap;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class JoinDao {
+	@Autowired
+	SqlSessionFactory factory;
 	
+	public int addMember(HashMap map){
+		SqlSession session = factory.openSession();
+		int r = 0;
+		try{
+			r = session.insert("join.insert", map);
+			session.commit();
+		}catch(Exception e){
+			e.printStackTrace();
+			session.rollback();
+		}finally{
+			session.close();
+		}
+		return r;
+	}
 }
