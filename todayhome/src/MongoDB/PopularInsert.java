497867680afcd5dcdd4ec014b5349475b90e8728
admin <admin@192.168.10.14>
2017-03-30 12:56:53 +0900

    mongoDB class 파일 생성

--- todayhome/src/MongoDB/PopularInsert.java
+++ todayhome/src/MongoDB/PopularInsert.java
@@ -7,18 +7,15 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MongoInsert {
+public class PopularInsert {
+	
 	@Autowired
 	MongoTemplate mt;
-	
-	// 검색결과 인서트
-	public void searchInsert(){
+	// 뷰꾸미기 용 가상 인서트
+	public void popularInsert(){
 		HashMap map = new HashMap();
-			String[] target={"서울","서울","","",""};
-			String[] startMonth={};
-			String[] endMonth={};
-			String[] peopleCnt={};
-			String[] id={}; // 있는거에서 
+			String[] hostingnum={}; // 있는거에서
+			String[] during={};
+			String[] viewcount={}; 
 	}
-
 }
